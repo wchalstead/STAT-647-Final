6bdf7c4df356026c9a034be38798dc0cdd580e3c
Wesley Halstead <wesley.halstead@tamu.edu>
2024-11-08 14:58:23 -0600

    Malaria inside Niger exploratory analysis

--- scripts/explorator_malaria.R
+++ scripts/explorator_malaria.R
@@ -4,7 +4,8 @@ library(fields)
 library(sf)
 library(ggplot2)
 library(maps)
-
+library(httr)
+library(jsonlite)
 
 test_data <- read.csv('data/malaria_by_country.csv')
 
@@ -16,8 +17,6 @@ test_data2 <- merge(test_data,spData::world, by.x = 'DIM_GEO_NAME', by.y = 'name
 
 head(test_data2)
 
-plot(test_data2)
-
 sf_data <- st_as_sf(test_data2)
 
 sf_data <- sf_data[sf_data$continent == 'Africa',]
@@ -27,6 +26,8 @@ ggplot(sf_data)+geom_sf(aes(fill=VALUE_NUMERIC))
 ggplot(sf_data)+geom_sf(aes(fill=log(1+VALUE_NUMERIC*pop/1000)))
 
 ggplot(sf_data)+geom_sf(aes(fill=(VALUE_NUMERIC*pop/1000)))
+
+ggplot(sf_data)+geom_sf(aes(fill=(gdpPercap)))
 
 
 A <- st_touches(sf_data)
@@ -47,3 +48,34 @@ map('world',regions = sf_data$DIM_GEO_NAME)
 plot.listw(nb1,coords=s,add=T,col=4,lwd=2)
 dev.off()
 
+
+
+#### Testing nbcost function
+
+x_test <- rbinom(41,1,0.5)
+
+sf_data_test <- sf_data
+sf_data_test$x_test <- x_test
+
+sf_data_test <- sf_data_test[rowSums(A) != 0,]
+
+ggplot(sf_data_test)+geom_sf(aes(fill=x_test))
+
+nb_test <- poly2nb(sf_data_test)
+
+lcosts <- nbcosts(nb_test, x_test)
+nb.w <- nb2listw(nb_test, lcosts, style="B")
+mst.bh <- mstree(nb.w,5)
+
+coords <- as.matrix(st_coordinates(s[rowSums(A) != 0,]))
+
+plot(sf_data$geom)
+plot(mst.bh, coords, col = 2, cex.lab = .6, cex.circles = 0.035, fg = 'blue', add = T)
+
+
+k=3 ## three clusters
+
+res1 <- skater(mst.bh[,1:2], dpad, k-1) 
+membership = res1$groups
+### groups size
+table(membership) 
