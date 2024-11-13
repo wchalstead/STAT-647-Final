702dc56d7f46e4ec9b0f271a16bbe6ad31163722
Wesley Halstead <wesley.halstead@tamu.edu>
2024-11-13 13:50:45 -0600

    More work on aereal clustering and fitting GLM and Spatial GLM models.

--- scripts/explore_scripts/data_by_location.R
+++ scripts/explore_scripts/data_by_location.R
@@ -5,9 +5,10 @@ library('here')
 library('ggplot2')
 library('fields')
 library('stringr')
+library('spmodel')
 
 ## Import data
-MISNiger <- read_dta('data/NIPR82FL.dta')
+MISNiger <- read_dta('data/NigerPointData/NIPR82FL.dta')
 
 
 ## Getting understanding
@@ -55,9 +56,65 @@ sf1Reduced <- sf1[,c('ID','geometry','LATNUM','LONGNUM')]
 df1 <- merge(sf1Reduced,MISReduced )
 
 
-quilt.plot(df1$LONGNUM, df1$LATNUM, df1$Malaria)
+quilt.plot(jitter(df1$LONGNUM), jitter(df1$LATNUM), df1$Malaria)
 
 ggplot(df1)+
-  geom_sf(aes(color = Malaria))
+  geom_point(aes(x = jitter(LONGNUM), y = jitter(LATNUM), color = Malaria))
 
 
+### Now need to extract geographic coefficients
+spatial_coef <- read.csv('data/NIGC81FL/NIGC81FL.csv')
+spatial_coef$ID <- as.numeric(str_sub(sf1$DHSID,-3))
+
+
+## Going to reduce this to less coefs
+spatial_coef <- spatial_coef[,c(131,130,97,71)]
+######################### Can change this ^ part later to include more variables
+
+df1 <- merge(df1, spatial_coef)
+
+
+plot(df1)
+
+
+##### Fit basic glm
+model1 <- glm(Malaria ~  Rainfall_2020+ Mean_Temperature_2020, 
+              data = df1, 
+              family = binomial)
+
+summary(model1)
+plot(df1$LATNUM, model1$residuals)
+plot(df1$LONGNUM, model1$residuals)
+
+quilt.plot(
+  (scale(df1$LONGNUM) - min(scale(df1$LONGNUM)))**0.5,
+  (scale(df1$LATNUM) - min(scale(df1$LATNUM)))**0.5,
+  model1$residuals
+)
+
+
+ggplot(cbind(df1, model1$residuals),aes(color = model1.residuals))+
+  geom_sf()+
+  scale_color_viridis_c(option = 'turbo')
+
+ggplot(cbind(df1, model1$residuals),aes(color = model1.residuals))+
+  geom_point(aes(x = jitter(LONGNUM, 250), y = jitter(LATNUM,250)))+
+  scale_color_viridis_c(option = 'turbo')
+
+ggplot(cbind(df1, residuals(model1)),aes(color = residuals.model1.))+
+  geom_point(aes(x = jitter(LONGNUM, 250), y = jitter(LATNUM,250)))+
+  scale_color_viridis_c(option = 'turbo')
+
+### Fit spatial glm
+model2 <- spglm(Malaria ~  Rainfall_2020+ Mean_Temperature_2020, 
+              data = df1, 
+              family = binomial,
+              spcov_type = 'matern')
+
+
+ggplot(cbind(df1, residuals(model2)),aes(color = residuals.model2.))+
+  geom_point(aes(x = jitter(LONGNUM, 250), y = jitter(LATNUM,250)))+
+  scale_color_viridis_c(option = 'turbo')
+
+plot(df1$LATNUM, residuals(model2))
+plot(df1$LONGNUM, residuals(model2))
